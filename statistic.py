72eba2ed9554ceeeea6cd31924b2bdbce4427a1e
CH-YSQ <112876130+CH-YSQ@users.noreply.github.com>
2024-03-11 05:58:46 +0100

    result

--- statistic.py
+++ statistic.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Number of workers
+# Number of instances
 x = ["1", "2", "4", "8"]
 
 # Average Req/Sec of three runs for each number of workers
 y = [815.59, 648.51, 543.88, 548.97]
 
-plt.title("Performance of service per number of workers")
-plt.xlabel("Number of workers")
+plt.title("Performance of service per number of instances")
+plt.xlabel("Number of Instances")
 plt.ylabel("Average Req/Sec")
 plt.bar(x, y)
 plt.show()
