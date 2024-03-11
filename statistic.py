eacb65fce3848101d2a7591ab6701ef8c4f8227c
CH-YSQ <112876130+CH-YSQ@users.noreply.github.com>
2024-03-11 06:10:18 +0100

    result

--- statistic.py
+++ statistic.py
@@ -6,8 +6,8 @@ x = ["1", "2", "4", "8"]
 # Average Req/Sec of three runs for each number of workers
 y = [814.59, 647.51, 544.88, 545.97]
 
-plt.title("Performance in Request/Seconds per number of instances")
+plt.ylabel("Average Request per Seconds")
 plt.xlabel("Number of Instances")
-plt.ylabel("Average Request/Seconds")
+plt.title("Performance in Request/Seconds per number of instances")
 plt.bar(x, y)
 plt.show()
